317c40aa1f46d522f2cce8fc4acb0d1553a187eb
Jan-Otto Kröpke <mail@jkroepke.de>
2024-07-27 23:26:48 +0200

    wip

--- pkg/changelog/main.go
+++ pkg/changelog/main.go
@@ -49,9 +49,11 @@ func (c *Changelog) SetRemote(remoteURL string) {
 	case regexpGithubRepoInfos.MatchString(remoteURL):
 		matches := regexpGithubRepoInfos.FindStringSubmatch(remoteURL)
 
-		c.links.compareURL = "https://github.com/%s/compare/%%s...%" + matches[1]
+		//nolint:perfsprint
+		c.links.compareURL = fmt.Sprintf("https://github.com/%s/compare/%%s...%%s", matches[1])
 		c.links.prURL = fmt.Sprintf("https://github.com/%s/pull/$1", matches[1])
-		c.links.commitURL = "https://github.com/%s/commit/%" + matches[1]
+		//nolint:perfsprint
+		c.links.commitURL = fmt.Sprintf("https://github.com/%s/commit/%%s", matches[1])
 	}
 }
 
